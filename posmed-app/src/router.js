23d1d6357053e7fc1378f31d4bd6e5cac3b5023c
Sóstenes Freitas <sostenesfreitas@gmail.com>
2017-05-15 19:01:24 +0000

    Update router.js

--- posmed-app/src/router.js
+++ posmed-app/src/router.js
@@ -9,13 +9,13 @@ function load (component) {
 
 export default new VueRouter({
   routes: [
-    { path: '/', component: load('Index') },
+    { path: '/', component: load('dashboardmedico') },
     { path: '/consulta', component: load('consulta') },
     { path: '/paciente', component: load('paciente') },
     { path: '/pacientes', component: load('pacientes') },
     { path: '/medico', component: load('medico') },
     { path: '/medicos', component: load('medicos') },
-    { path: '/dashdoctor', component: load('dashboardmedico') },
+    { path: '/painel', component: load('Index') },
     { path: '*', component: load('Error404') } // Not found
   ]
 })
